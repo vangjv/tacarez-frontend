c76bd6f9dc9d8dddbe9bcdd27d004f4513fb138f
vangjv <jonathan@meliority.solutions>
2021-08-09 00:50:07 -0500

    Added open map feature

--- src/app/app.menu.component.ts
+++ src/app/app.menu.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-menu',
@@ -8,13 +10,37 @@ import {Component, OnInit } from '@angular/core';
                 <li app-menuitem *ngFor="let item of model; let i = index;" [item]="item" [index]="i" [root]="true"></li>
             </ul>
         </div>
+        <p-dialog header="Open Map Feature" [(visible)]="showOpenFeature" [style]="{width: '50vw'}">
+        <div [formGroup]="openMapFeatureForm">
+            <div class="grid" >
+                <div class="col-12 md:4">
+                    <div class="p-fluid">
+                        <div class="p-field mb-3">
+                            <label class="mr-2">Feature name</label>
+                            <input type="Text" pInputText [style]="{'width': '100%'}" formControlName="featureName">
+                        </div>                      
+                    </div>
+                </div>
+            </div>
+            <p-footer>
+                <div class="flex justify-content-end">
+                    <button pButton (click)="openMapFeature()" [disabled]="openMapFeatureForm.invalid">Open</button>
+                </div>
+            </p-footer>
+        </div>    
+    </p-dialog>
     `
 })
 export class AppMenuComponent implements OnInit {
-
+    openMapFeatureForm:FormGroup;
+    showOpenFeature:boolean = false;
     model: any[];
+    constructor(private router:Router) {
+
+    }
 
     ngOnInit() {
+        this.openMapFeatureForm = this.createOpenMapFeatureForm();
         this.model = [
             // {label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/']},
             {
@@ -27,7 +53,9 @@ export class AppMenuComponent implements OnInit {
                 label: 'New Feature',  routerLink: ['/new']
             },
             {
-                label: 'Open Feature',  routerLink: ['/openfeature']
+                label: 'Open Feature', command: ()=>{
+                    this.showOpenFeature = true;
+                }
             },
             {
                 label: 'Recent',  routerLink: ['/recent']
@@ -36,5 +64,17 @@ export class AppMenuComponent implements OnInit {
                 label: 'Explore',  routerLink: ['/explore']
             }
         ];
+    }
+
+    createOpenMapFeatureForm():FormGroup{
+        return new FormGroup({
+          featureName: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z0-9 -]*')]),
+        });
+    }
+
+    openMapFeature(){
+        this.showOpenFeature = false;
+        this.router.navigateByUrl("feature/" + this.openMapFeatureForm.value.featureName.replace(" ", "-"));
+        this.openMapFeatureForm.reset();
     }
 }
